ffa8fd4ca922a011088db73ccba34850a8064ef8
holly Switzer <holly12ann@gmail.com>
2018-02-28 10:51:40 -0600

    Added group memebers dominion files for assignment 5

---
+++ projects/switzerh/dujuDominion/dominion_helpers.h
@@ -0,0 +1,20 @@
+#ifndef _DOMINION_HELPERS_H
+#define _DOMINION_HELPERS_H
+
+#include "dominion.h"
+
+int drawCard(int player, struct gameState *state);
+int updateCoins(int player, struct gameState *state, int bonus);
+int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+		int trashFlag);
+int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
+int getCost(int cardNumber);
+int cardEffect(int card, int choice1, int choice2, int choice3, 
+	       struct gameState *state, int handPos, int *bonus);
+
+int cardEffectSmithy(int currentPlayer, struct gameState *state, int handPos); 
+int cardEffectAdventurer(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectMine(int currentPlayer, struct gameState *state, int choice1, int choice2, int handPos);
+int cardEffectVillage(int currentPlayer, struct gameState *state, int handPos);
+int cardEffectRemodel(int currentPlayer, struct gameState *state, int choice1, int choice2, int handPos);
+#endif
